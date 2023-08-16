8b52d57cc300a0ef83bb4cf0fc779bdca5d034ed
patrickgatiri <patrickgatiri@gmail.com>
2023-08-16 15:42:01 +0300

    update service name on init script

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-docker-compose run --rm ovpn ovpn_genconfig -u udp://192.168.88.7
-docker-compose run --rm ovpn ovpn_initpki
+docker-compose run --rm openvpn-server ovpn_genconfig -u udp://192.168.88.7
+docker-compose run --rm openvpn-server ovpn_initpki
